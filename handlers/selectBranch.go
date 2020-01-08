bbf841bd3eaf1fb8782bacb0ef8bf96132b63807
kiris <kiris60@gmail.com>
2020-01-09 01:46:29 +0900

    ..

---
+++ handlers/selectBranch.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"github.com/nlopes/slack"
+	"github.com/pkg/errors"
+	"net/http"
+
+	"github.com/kiris/brownie/components"
+	"github.com/kiris/brownie/models"
+)
+
+type SelectBranchHandler struct {
+	Workspace *models.Workspace
+}
+
+func (h *SelectBranchHandler) ServInteraction(w http.ResponseWriter, callback *slack.InteractionCallback) error {
+	component := components.NewMakeComponentFromInteraction(callback, true)
+
+	selectedRepository := component.SelectedRepository()
+	repository := h.Workspace.Repository(selectedRepository)
+	if repository == nil {
+		return errors.Errorf("failed to exec make command. repository not found: name = %s", selectedRepository)
+	}
+	targets, _ := repository.CollectMakeTargets()
+	component.AppendSelectTargetAttachment(targets)
+
+	renderer := components.InteractionRenderer{
+		Writer:   w,
+		Callback: callback,
+	}
+	return renderer.Render(component)
+}
